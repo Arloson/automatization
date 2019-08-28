318413de4f27724ff6b7bacdd3af90bc1099baf0
Sergey <sergeyantonov3083@gmail.com>
2019-08-28 11:06:48 +0300

    add parce file ip_host

--- exp.sh
+++ exp.sh
@@ -1,26 +1,28 @@
 #! /usr/bin/expect -f
 set timeout -1
 
-proc scat_ip { START_IP } {
-	spawn ping -c 1 $START_IP
-	
-
-}
 
 proc read_ip {} {
 	set f [open "ip_host.txt"]
-	set ip [split [read $f] "\n"]
+	#set ip [split [read $f] "\n"]
+	#set len [llength $ip]
+	#puts "$len"
+	
+	while { [gets $f line] >= 0 } {
+		run_ssh $line
+	}
+
 	close $f
-	set sizeip [ string length ip ]
-	foreach i $ip {
-		puts "$i"
-		}		
+
+	#foreach i $ip {
+	#	run_ssh $i
+	#	}		
 }
 
 
 proc run_ssh { MY_COUNT } {
 set DRWEB "/opt/drweb/drweb"
-spawn ssh root@192.168.3.$MY_COUNT 
+spawn ssh root@$MY_COUNT 
 expect	{
 	"*es/no)" {
        	send  "yes\r"
@@ -29,17 +31,15 @@ expect	{
 	send "12345678\r"
 }
 }	
-#expect 	"password:"
-#send  "12345678\r"
 
 expect "#*"
-send "$DRWEB\r"
-expect "#*"
+send "/scan.sh &\r"
+expect "#*\r"
 send "exit\r"
 }
 
 set COUNT 1
-#run_ssh $COUNT
+
 read_ip
 
 expect eof
